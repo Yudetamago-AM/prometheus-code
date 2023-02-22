7477f86a055ac5b658a7427cf3a2e9cdf9a4adf3
Yudetamago-AM <akio@rittai.org>
2023-02-22 17:22:19 +0900

    add lib

--- lib/icm20948/examples/ex1_read_raw/ex1_read_raw.c
+++ lib/icm20948/examples/ex1_read_raw/ex1_read_raw.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 #include "pico/stdlib.h"
-#include "../../lib/icm20948/src/pico-icm20948.h"
+#include "hardware/i2c.h"
+#include "../../src/pico-icm20948.h"
 
 i2c_inst_t icm20948_i2c = {i2c1_hw, false};
 icm20948_config_t config = {0x68, 0x0C, &icm20948_i2c};
 icm20984_data_t data;
-
 
 int main(void) {
     stdio_init_all();
@@ -22,10 +22,10 @@ int main(void) {
     float accel_g[3] = {0}, gyro_dps[3] = {0}, mag_ut[3] = {0}, temp_c = 0;
 
     while(1) {
-        //icm20948_read_raw_accel(&config, accel_raw);
-        //icm20948_read_raw_gyro(&config, gyro_raw);
+        icm20948_read_raw_accel(&config, accel_raw);
+        icm20948_read_raw_gyro(&config, gyro_raw);
         icm20948_read_raw_mag(&config, mag_raw);
-        //icm20948_read_temp_c(&config, &temp_c);
+        icm20948_read_temp_c(&config, &temp_c);
 
         for (uint8_t i = 0; i < 3; i++) {
             accel_g[i] = (float)accel_raw[i] / 16384.0f;
@@ -40,10 +40,10 @@ int main(void) {
         // ex) if full_scale == +-250dps then gyro = raw_value / (65535 / 500) = raw_value / 131
         // mag(uT)    = raw_value / (32752 / 4912) = (approx) (raw_value / 20) * 3
         // temp  = ((raw_value - ambient_temp) / speed_of_sound) + 21
-        //printf("accel. x: %+2.5f, y: %+2.5f, z:%+2.5f\n", accel_g[0], accel_g[1], accel_g[2]);
-        //printf("gyro.  x: %+2.5f, y: %+2.5f, z:%+2.5f\n", gyro_dps[0], gyro_dps[1], gyro_dps[2]);
-        printf("%04d,%04d,%04d\n", mag_raw[0], mag_raw[1], mag_raw[2]);
-        //printf("temp: %+2.5f\n", temp_c);
+        printf("accel. x: %+2.5f, y: %+2.5f, z:%+2.5f\n", accel_g[0], accel_g[1], accel_g[2]);
+        printf("gyro.  x: %+2.5f, y: %+2.5f, z:%+2.5f\n", gyro_dps[0], gyro_dps[1], gyro_dps[2]);
+        printf("mag.   x: %+2.5f, y: %+2.5f, z:%+2.5f\n", mag_ut[0], mag_ut[1], mag_ut[2]);
+        printf("temp: %+2.5f\n", temp_c);
         #endif
         sleep_ms(10);
     }
